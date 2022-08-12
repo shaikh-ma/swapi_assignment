bc6723a4bfe8ffdda4659b3d24e1af2aabb97400
shaikh-ma <88078876+shaikh-ma@users.noreply.github.com>
2022-08-12 15:37:14 +0530

    Adding the main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    try:
+        import requests
+        import pandas as pd
+    except ModuleNotFoundError:
+        import "Assignment - with standard libraries"
+    else:
+        import Assignment
+    finally:
+        pass
